c78f53ca541e8ba745e8a8b5b5e305ccbb497fbf
elena <elenagovi0@gmail.com>
2025-03-28 18:21:45 +0100

    Added absolute error as metric
    Given that our use-case is robotic picking, it is important to know the precise absolute error in terms of unit measure (mm, cm, dm or m).
    
    signed-off by Elena Govi <elenagovi0@gmail.com>

--- metric_depth/util/metric.py
+++ metric_depth/util/metric.py
@@ -13,6 +13,8 @@ def eval_depth(pred, target):
     diff = pred - target
     diff_log = torch.log(pred) - torch.log(target)
 
+    abs_err = torch.mean(torch.abs(diff))
+
     abs_rel = torch.mean(torch.abs(diff) / target)
     sq_rel = torch.mean(torch.pow(diff, 2) / target)
 
@@ -22,5 +24,5 @@ def eval_depth(pred, target):
     log10 = torch.mean(torch.abs(torch.log10(pred) - torch.log10(target)))
     silog = torch.sqrt(torch.pow(diff_log, 2).mean() - 0.5 * torch.pow(diff_log.mean(), 2))
 
-    return {'d1': d1.item(), 'd2': d2.item(), 'd3': d3.item(), 'abs_rel': abs_rel.item(), 'sq_rel': sq_rel.item(), 
+    return {'d1': d1.item(), 'd2': d2.item(), 'd3': d3.item(), 'abs_err': abs_err.item(), 'abs_rel': abs_rel.item(), 'sq_rel': sq_rel.item(), 
             'rmse': rmse.item(), 'rmse_log': rmse_log.item(), 'log10':log10.item(), 'silog':silog.item()}
