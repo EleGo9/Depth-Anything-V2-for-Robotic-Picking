8bca455bec11382cb915e6d5e64cc3388045af19
Davide Sapienza <sapienza.dav@gmail.com>
2025-05-29 14:04:04 +0200

    added evaluation only code

--- metric_depth/sygrid_test.sh
+++ metric_depth/sygrid_test.sh
@@ -1,16 +1,15 @@
 #!/bin/bash
 now=$(date +"%Y%m%d_%H%M%S")
 
-epoch=100
 bs=1
 gpus=1
-lr=0.000005
 encoder=vits
 dataset=sygrid # vkitti
 img_size=518
 min_depth=0.001 #1 millimeters
 max_depth=0.80 # NOTE: # 80 m for virtual kitti
 pretrained_from=../checkpoints/depth_anything_v2_${encoder}.pth
+load_from=exp/sygrid_lr0_000005_maxd0_8m_ep120/latest.pth
 save_path=exp/sygrid_lr0_000005_maxd0_8m_ep120 # exp/vkitti remember to change the name into 80cm
 
 mkdir -p $save_path
@@ -24,6 +23,6 @@ python3 -m torch.distributed.launch \
     --node_rank=0 \
     --master_addr=localhost \
     --master_port=20596 \
-    train.py --epoch $epoch --encoder $encoder --bs $bs --lr $lr --save-path $save_path --dataset $dataset \
+    val.py --encoder $encoder --bs $bs --dataset $dataset --load_from $load_from\
     --img-size $img_size --min-depth $min_depth --max-depth $max_depth --pretrained-from $pretrained_from \
     --port 20596 2>&1 | tee -a $save_path/$now.log
